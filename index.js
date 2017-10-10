4b424a196328c174b7044df78f8ada99e00fcf96
Maks <ush.maksim@gmail.com>
2017-10-10 11:38:57 +0300

    Add Solution

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
+/**
+ *  Resolve given value after delay;
+ *
+ *  @param Mixed value
+ *  @param Number delay, seconds (optional default 1s)
+ *
+ *  @return Promise
+ */
+const secondify = (value, delay=1) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(value), delay * 1000)
+    });
+}
+
+/**
+ * Show given after delay
+ *
+ * @param Mixed value
+ * @param Number delay, seconds
+ */
+async function showWithDelay (value, delay) {
+    console.log(await secondify(value, delay))
+}
 
 /**
  * Show each array items with delay
  *
  * @param Array arr
- * @param Number delay, ms
+ * @param Number delay, seconds (optional default 1s)
  */
-export default (arr, delay) => {
+const showDelayedItems = (arr, delay = 1) => {
+    let promise = Promise.resolve();
+    arr.forEach((item) => promise = promise
+        .then(() => showWithDelay(item,delay)));
+}
 
+export default showDelayedItems;
+export {secondify, showWithDelay, showDelayedItems};
+
+// demo mode
+// use:
+// node --require babel-register index.js
+if(!module.parent) {
+    let demoArray = [1,'2',[3],{item: 4}, true];
+    showDelayedItems(demoArray);
 }
--- test/promise.test.js
+++ test/promise.test.js
@@ -1,9 +1,66 @@
 import {expect} from 'chai';
-import {sinon} from 'sinon';
+import sinon from 'sinon';
 
-import showItem from '../index';
+import {secondify, showwithDelay, showDelayedItems} from '../index';
 
 describe('Lectrum contest', () => {
+
+    context('Used functions', () => {
+        it('Array.prototype.forEach was been used', () => {
+            const testArray = [1, "2", [3]];
+            let each = sinon.stub(testArray, 'forEach');
+
+            showDelayedItems(testArray, 0);
+            
+            expect(each.called).to.be.true;
+            each.restore();
+        });
+
+        it('Promice was been used', () => {
+            let promise = sinon.spy(Promise, 'resolve');
+            let testArray = [1, 2];
+
+            showDelayedItems(testArray, 0);
+
+            expect(promise.called).to.be.true
+
+            promise.restore();
+        });
+     });
+
+    context('Behavior', () => {
+
+        let clock, log;
+        const testArray = [1,"2", [3], 4, 5];
+
+        beforeEach(() => {
+            clock = sinon.useFakeTimers();
+            log = sinon.spy(console, 'log');
+        });
+
+        afterEach( () => {
+            clock.restore();
+            log.restore();
+        });
+
+        it('Show next item with delay 1s',  (done) => {
+
+            showDelayedItems(testArray, 1)
+
+            setTimeout( () => {
+                expect(log.called).to.be.true;
+                console.log(log);
+                done();
+            }, 1000);
+            
+            clock.tick(1000);
+        });
+
+        it('Show only one item after 1s', () => {
+        
+        });
+
+    });
 
 });
 
